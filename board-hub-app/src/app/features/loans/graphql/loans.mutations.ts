f58ab6be3ea06a16b8d5c890ac817897303a0d6e
Franz-Gonzales <gonzalesfranzreinaldo@gmail.com>
2026-03-03 02:55:57 -0400

    Modulo de Loans completada en el front

---
+++ board-hub-app/src/app/features/loans/graphql/loans.mutations.ts
@@ -0,0 +1,83 @@
+import { gql } from 'apollo-angular';
+
+export const CREATE_LOAN = gql`
+  mutation CreateLoan($createLoanInput: CreateLoanInput!) {
+    createLoan(createLoanInput: $createLoanInput) {
+      id
+      gameId
+      clientId
+      quantity
+      startDate
+      endDate
+      deliveryDate
+      status
+      pricePerDay
+      totalPrice
+      notes
+      isDeleted
+      createdAt
+      updatedAt
+      deletedAt
+      game {
+        id
+        title
+        category
+        imageUrl
+        pricePerDay
+        stockAvailable
+      }
+      client {
+        id
+        name
+        lastName
+        ci
+        phoneNumber
+        email
+      }
+    }
+  }
+`;
+
+export const RETURN_LOAN = gql`
+  mutation ReturnLoan($id: ID!) {
+    returnLoan(id: $id) {
+      id
+      gameId
+      clientId
+      quantity
+      startDate
+      endDate
+      deliveryDate
+      status
+      pricePerDay
+      totalPrice
+      notes
+      isDeleted
+      createdAt
+      updatedAt
+      deletedAt
+      game {
+        id
+        title
+        category
+        imageUrl
+        pricePerDay
+        stockAvailable
+      }
+      client {
+        id
+        name
+        lastName
+        ci
+        phoneNumber
+        email
+      }
+    }
+  }
+`;
+
+export const DELETE_LOAN = gql`
+  mutation RemoveLoan($id: ID!) {
+    removeLoan(id: $id)
+  }
+`;
